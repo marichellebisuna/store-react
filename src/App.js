57b8eb27770c860ff5334b56009bfcf43bed6ab2
Marichelle Bisuna <marichellebisuna@yahoo.com>
2019-02-05 11:28:30 +1100

    Components created

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import"bootstrap/dist/css/bootstrap.min.css";
+import Cart from './components/Cart';
+import Default from './components/Default';
+import ProductList from './components/ProductList';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">column One</div>
-          <div className="col-6"><span>
-            <i className="fas fa-home" /></span></div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Cart />
+        <Default />
+        <Details />
+        <ProductList />
+      </React.Fragment>    
     );
   }
 }
